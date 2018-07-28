2e95bce6de6a900e55014ed58d9e17ff7228f671
Nathalyn Nunoo <nnunoo@Nathalyns-MBP.attlocal.net>
2018-07-28 16:24:52 -0700

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
+module.exports = {
+  siteMetadata: {
+    title: 'title',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
